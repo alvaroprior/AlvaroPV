f6215a80e5ae0c462da6138429da0119f6a7ccd1
alvaroprior <Alvaro.prior98@gmail.com>
2017-03-13 08:45:05 +0100

    cambio proyecto 2

--- src/alvaropv/AlvaroPV.java
+++ src/alvaropv/AlvaroPV.java
@@ -10,6 +10,7 @@ public class AlvaroPV {
     public static void main(String[] args) {
     System.out.println("inicio de proyecto");
         System.out.println("cambio de proyecto de proyecto");
+         System.out.println("cambio de proyecto de proyecto 2");
 }
     
 }
